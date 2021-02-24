6fb40f97cb9106ee6c4a0cfd1ece86d91d44ac45
Aaron Powell <aaron@lunadigital.tv>
2021-02-24 15:27:23 -0500

    BtoA now supports rendering with Arnold 6.0.1.0 to 6.2.0.x on Windows, partially addresses issue #54

--- drivers/build_windows.bat
+++ drivers/build_windows.bat
@@ -1,1 +1,5 @@
-cl /LD src/driver_display_callback.cpp /I %ARNOLD_ROOT%/include %ARNOLD_ROOT%/lib/ai.lib /link /out:build/driver_display_callback.dll
+set ARNOLD_SDK="C:\path\to\Arnold-6.0.1.0-windows"
+cl /LD src/driver_display_callback.cpp /I %ARNOLD_SDK%/include %ARNOLD_SDK%/lib/ai.lib /link /out:build/driver_display_callback_v6.0.1.dll
+
+set ARNOLD_SDK="C:\path\to\Arnold-6.2.0.0-windows"
+cl /LD src/driver_display_callback.cpp /I %ARNOLD_SDK%/include %ARNOLD_SDK%/lib/ai.lib /link /out:build/driver_display_callback_v6.2.dll
