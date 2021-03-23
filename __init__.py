a8a0748c0fb56d59cb51997596b6cd422fb82263
Aaron Powell <aaron@lunadigital.tv>
2021-03-22 21:09:20 -0400

    Updated add-on version

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ bl_info = {
     "name": "Arnold Render Engine",
     "description": "Unofficial Arnold renderer integration",
     "author": "Luna Digital, Ltd.",
-    "version": (0, 2, 13),
+    "version": (0, 3, 0),
     "blender": (2, 83, 0),
     "category": "Render"
 }
