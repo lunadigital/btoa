1130f46ebc5ff39d3ece48beb428f0e03ff0d665
Aaron Powell <aaron@lunadigital.tv>
2021-05-17 21:31:15 -0400

    World select dropdown now works as expected #101

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ bl_info = {
     "name": "Arnold Render Engine",
     "description": "Unofficial Arnold renderer integration",
     "author": "Luna Digital, Ltd.",
-    "version": (0, 3, 0),
+    "version": (0, 3, 5),
     "blender": (2, 83, 0),
     "category": "Render"
 }
