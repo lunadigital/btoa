d786f7b0d50ba905ceb60f2ec0f3f9e4fc059001
Aaron Powell <aaron@lunadigital.tv>
2021-05-24 12:40:21 -0400

    Updated macOS display drivers

--- drivers/build_macos.sh
+++ drivers/build_macos.sh
@@ -1,5 +1,5 @@
-ARNOLD_SDK=/Users/aaronpowell/Desktop/Arnold-6.0.1.0-darwin
+ARNOLD_SDK=/Applications/Autodesk/Arnold-6.0.1.0-darwin
 c++ src/driver_display_callback.cpp -o build/driver_display_callback_v6.0.1.dylib -Wall -O2 -shared -fPIC -I$ARNOLD_SDK/include -L$ARNOLD_SDK/bin -lai -std=gnu++11
 
-ARNOLD_SDK=/Users/aaronpowell/Desktop/Arnold-6.2.0.0-darwin
+ARNOLD_SDK=/Applications/Autodesk/Arnold-6.2.0.0-darwin
 c++ src/driver_display_callback.cpp -o build/driver_display_callback_v6.2.dylib -Wall -O2 -shared -fPIC -I$ARNOLD_SDK/include -L$ARNOLD_SDK/bin -lai -std=gnu++11
