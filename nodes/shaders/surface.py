9593cff40715faf65b8fa07da9e96cff641d8b74
Aaron Powell <aaron@lunadigital.tv>
2022-06-21 13:02:20 -0400

    Converts internal parameters to sockets in AiAmbientOcclusion #115

--- nodes/shaders/surface.py
+++ nodes/shaders/surface.py
@@ -13,34 +13,26 @@ class AiAmbientOcclusion(bpy.types.Node, base.ArnoldNode):
     bl_label = "Ambient Occlusion"
     ai_name = "ambient_occlusion"
 
-    samples: IntProperty(name="Samples", min=1, default=3)
-    spread: FloatProperty(name="Spread", min=0, max=1, default=1)
-    near_clip: FloatProperty(name="Near Clip", min=0)
-    far_clip: FloatProperty(name="Far Clip", min=0, default=5)
     invert_normals: BoolProperty(name="Invert Normals")
     self_only: BoolProperty(name="Self Only")
 
     def init(self, context):
-        self.inputs.new("AiNodeSocketRGB", "Black Color", identifier="black").default_value = (0, 0, 0)
-        self.inputs.new("AiNodeSocketRGB", "White Color", identifier="white")
+        self.inputs.new('AiNodeSocketIntAboveOne', "Samples", identifier="samples").default_value = 3
+        self.inputs.new('AiNodeSocketFloatNormalized', "Spread", identifier="spread").default_value = 1
         self.inputs.new('AiNodeSocketFloatPositive', "Falloff", identifier="falloff").default_value = 1
+        self.inputs.new('AiNodeSocketFloatAboveOne', "Near Clip", identifier="near_clip")
+        self.inputs.new('AiNodeSocketFloatAboveOne', "Far Clip", identifier="far_clip").default_value = 100
+        self.inputs.new("AiNodeSocketRGB", "White", identifier="white").default_value = (1, 1, 1)
+        self.inputs.new("AiNodeSocketRGB", "Black", identifier="black").default_value = (0, 0, 0)
         self.inputs.new('AiNodeSocketVector', "Normal", identifier="normal")
 
         self.outputs.new('AiNodeSocketSurface', name="RGB", identifier="output")
 
     def draw_buttons(self, context, layout):
-        layout.prop(self, "samples")
-        layout.prop(self, "spread")
-        layout.prop(self, "near_clip")
-        layout.prop(self, "far_clip")
         layout.prop(self, "invert_normals")
         layout.prop(self, "self_only")
 
     def sub_export(self, node):
-        node.set_uint("samples", self.samples)
-        node.set_float("spread", self.spread)
-        node.set_float("near_clip", self.near_clip)
-        node.set_float("far_clip", self.far_clip)
         node.set_bool("invert_normals", self.invert_normals)
         node.set_bool("self_only", self.self_only)
 
