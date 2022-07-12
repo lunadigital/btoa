e0ae7893e1d951468f69af71e4be0a1d1d36ad3b
Aaron Powell <aaron@lunadigital.tv>
2022-07-12 16:05:53 -0400

    Cleans up camera cache

--- btoa/camera_cache.py
+++ btoa/camera_cache.py
@@ -9,28 +9,6 @@ class CameraCache:
         self.clip_start = 0.01
         self.clip_end = 1000
 
-    def __matrix_changed(self, camera):
-        return self.matrix_world != camera.matrix_world
-
-    def __ortho_scale_changed(self, camera):
-        return camera.data.arnold.camera_type == "ortho_camera" and camera.data.ortho_scale != self.ortho_scale
-    
-    def __view_zoom_changed(self, camera):
-        return self.zoom != camera.data.zoom
-    
-    def __view_offset_changed(self, camera):
-        return self.offset != tuple(list(camera.data.offset))
-
-    def __camera_settings_changed(self, camera):
-        if (
-            self.angle != camera.data.angle
-            or self.clip_start != camera.data.clip_start
-            or self.clip_end != camera.data.clip_end
-        ):
-            return True
-
-        return False
-
     def sync(self, camera):
         self.matrix_world = camera.matrix_world
         self.ortho_scale = camera.data.ortho_scale
@@ -42,13 +20,17 @@ class CameraCache:
         self.clip_end = camera.data.clip_end
 
     def redraw_required(self, camera):
-        if (
-            self.__matrix_changed(camera)
-            or self.__ortho_scale_changed(camera)
-            or self.__view_zoom_changed(camera)
-            or self.__view_offset_changed(camera)
-            or self.__camera_settings_changed(camera)
-        ):
+        params = [
+            self.matrix_world != camera.matrix_world,
+            self.ortho_scale != camera.data.ortho_scale and camera.data.arnold.camera_type == "ortho_camera",
+            self.zoom != camera.data.zoom,
+            self.offset != tuple(list(camera.data.offset)),
+            self.angle != camera.data.angle,
+            self.clip_start != camera.data.clip_start,
+            self.clip_end != camera.data.clip_end
+        ]
+
+        if any(params):
             return True
 
         return False
