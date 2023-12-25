185c0fb85d4a482b75e17b49f0f7c96766782376
Aaron Powell <aaron@lunadigital.tv>
2023-12-24 23:18:37 -0500

    Autoswitches shader node editors to/from Arnold when thecrender engine changes for ease

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -2,15 +2,18 @@ from . import core
 from . import sockets
 from . import shaders
 from . import menus
+from . import autoswitch
 
 def register():
     core.register()
     sockets.register()
     shaders.register()
     menus.register()
+    autoswitch.register()
 
 def unregister():
     core.unregister()
     sockets.unregister()
     shaders.unregister()
     menus.unregister()
+    autoswitch.unregister()
